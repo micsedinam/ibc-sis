c958984078c77426fd5ff6869ac8e19d6c3f7dc2
Tyro <tyro.rrico@gmail.com>
2017-07-06 05:10:46 +0000

    3. Parent can only see Approved student's Records. clean notiications

--- app/Http/Controllers/parentResultController.php
+++ app/Http/Controllers/parentResultController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Results;
 use App\User;
+use App\Studentparent;
+use Auth;
 
 class parentResultController extends Controller
 {
@@ -45,30 +47,67 @@ class parentResultController extends Controller
 
     public function view(Request $request)
     {
+         $myresult = $request['studentid'];
+       $student = User::select('users')
+            ->where('studentid', '=', $myresult)
+            ->select('id','firstname', 'surname', 'programme', 'studentid')
+            ->get()->first()
+        ;
 
-        $myresult = $request['studentid'];
-
-        $result = Results::select('*')
-            ->where('studentid','=', $myresult)
-            ->latest()
+        //dd($student);
+        if ($student != null) {
+            $mine = Studentparent::where('parent_id', '=', Auth::user()->id)
+            ->where('stud_id', '=', $student->id)
             ->get()
-        ;
+            ->first();
 
-        $results = Results::select('*')
-        	->where('studentid', '=', $myresult)
-        	->latest()
-        	->get()
-        ;	
+            //dd($mine);
+            if ($mine != null ) {
+                if ($mine->state === 'approved') {
+                     $result = Results::select('*')
+                        ->where('studentid','=', $myresult)
+                        ->latest()
+                        ->get()
+                    ;
 
-        $student = User::select('users')
-        				->where('studentid', '=', $myresult)
-        				->select('firstname', 'surname', 'programme', 'studentid')
-        				->get()
-        ;
+                    $results = Results::select('*')
+                        ->where('studentid', '=', $myresult)
+                        ->latest()
+                        ->get()
+                    ;   
 
-        //dd($result, $results, $student);
+                    $student = User::select('users')
+                                    ->where('studentid', '=', $myresult)
+                                    ->select('firstname', 'surname', 'programme', 'studentid')
+                                    ->get()
+                    ;
 
-        return view ('guardian.results') ->with ('result', $result) ->with ('results', $results) ->with ('student', $student);
+                    //dd($result, $results, $student);
+
+                    return view ('guardian.results') ->with ('result', $result) ->with ('results', $results) ->with ('student', $student);
+                } else {
+                    flash('Your ward request has not yet been approved.' )->error();
+                    return redirect('guardian/results');
+                }
+                
+            }else{
+                flash('This is not your ward check the ID and try again')->error();
+                
+               return redirect('guardian/results');
+
+            }
+
+         }else{
+
+            flash('Student does not exist')->error();
+            
+            return redirect('guardian/results');
+
+        }
+
     }
 
 }
+
+
+       
