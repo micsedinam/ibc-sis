270c9768a303b0d2373071f6bc60a466aa432f4f
Tyro <tyro.rrico@gmail.com>
2017-07-07 16:42:45 +0000

    Final on approving Chamge

--- app/Http/Controllers/approveController.php
+++ app/Http/Controllers/approveController.php
@@ -27,11 +27,11 @@ class approveController extends Controller
      */
     public function index()
     {
-        $request = Resultupdate::select('*', \DB::raw('resultupdates.ca_score as new_ca, resultupdates.exam_score as new_exam '))
+        $request = Resultupdate::select('*', \DB::raw('resultupdates.ca_score as new_ca, resultupdates.exam_score as new_exam ,resultupdates.state as state_ '))
             ->join('results',  'resultupdates.resultid', '=',  'results.id')
             ->join('resultchanges', 'resultupdates.requestid', '=' ,'resultchanges.id')
             ->get();
-       // dd($request);
+        //dd($request);
 
         return view('admin.results-change', compact('request'));
     }
@@ -54,7 +54,73 @@ class approveController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $pdate = $request['update'];
+       // return $request->all();
+        //dd($request->update);
+        $results = Results::findOrFail($request->result);
+
+        //Calculate total
+        $total  = $request['new_ca'] + $request['new_exam'];
+
+        //Calculate the grade
+        switch ($grade = $total) {
+            case $total >=80 || $total==100:
+               $grade =  "A1";
+                break;
+            case $total>=75:
+                $grade = "B2";
+                break;
+            case $total >=70:
+                $grade = "B3";
+                break;
+            case $total >=65:
+                $grade = "C4";
+                break;
+            case $total >=60:
+                $grade = "C5";
+                break;
+            case $total >=55:
+                $grade = "C6";
+                break;
+            case $total >=50:
+                $grade = "D7";
+                break;
+            case $total >=40:
+                $grade = "E8";
+                break;
+            case $total <39 && $total==0:
+                $grade = "F9";
+                break;
+        }
+
+        $results->ca_score = $request['new_ca'];
+        $results->exam_score = $request['new_exam'];
+        $results->total = $total;
+        $results->grade = $grade;
+
+       // dd($request->update);
+        if ($results->update()){
+           
+            //dd($request->requested);
+            $request = Resultchange::findOrFail($request->requested);
+            $request->state = 'updated';
+            //dd($request);
+            $request->update();
+
+            $update = Resultupdate::findOrFail($pdate);
+            $update->state = 'approved';
+                        //dd($update);
+
+            $update->update();
+
+            //echo 'saved';
+             flash('Results successfully updated.')->success();
+        }else{
+            flash('Results not saved.')->error();
+            //echo 'Not saved';
+        }
+
+        return redirect('/admin/approve');
     }
 
     /**
--- resources/views/admin/results-change.blade.php
+++ resources/views/admin/results-change.blade.php
@@ -15,42 +15,46 @@
 @section('content') 
 	<div class="row">
 		@foreach ($request as $item)
-			<div class="col-lg-3">
+			@if ($item->state_ === 'pending')
+				<div class="col-lg-3">
 	        <div class="panel panel-primary">
 	         <div class="panel-body">
 	         <p><strong>Approve Changes</strong></p>
 	            <hr>
-	               <h6>Student ID : {{ $item->studid }} <h6>
-	               <h6>Request : {{ $item->request }}</h6>
-	               <h6>Old: CA = {{ $item->ca_score }} Exam = {{ $item->exam_score }}</h6>
-	               <h6>New: CA = {{ $item->new_ca }} Exam =  {{ $item->new_exam}}</h6>
+	               <h6> <small>Student ID :</small> {{ $item->studid }} <h6>
+	               <h6><small>Request :</small> {{ $item->request }}</h6>
+	               <h6><small>Old: CA =</small> {{ $item->ca_score }} <small>Exam =</small> {{ $item->exam_score }}</h6>
+	               <h6><small>New: CA =</small> {{ $item->new_ca }} <small>Exam = </small>  {{ $item->new_exam}}</h6>
 				 <hr>
 	               <div class="row">
 	               	<div class="col-md-6">
-	               		<form action="{{ route('approve.store') }}" >
+	               		<form action="{{ route('approve.store') }}" method="POST" >
 	               	{{ csrf_field() }}
 
-	               	<input type="text" name="request" hidden>
-	               	<input type="text" name="update" hidden>
-	               	<input type="text" name="id" hidden>
+	               	<input type="text" name="result" value="{{ $item->resultid }}" hidden>
+	               	<input type="text" name="update" value="{{ $item->id }}" hidden>
+	               	<input type="text" name="requested" value="{{ $item->requestid }}" hidden>
+	               	<input type="text" name="new_ca" value="{{ $item->new_ca }}" hidden>
+	               	<input type="text" name="new_exam" value="{{ $item->new_exam}}" hidden>
 	               	<button class="btn btn-xs btn-primary">Approve</button>
 	               </form>
 	             
 	               	</div>
 	               	<div class="col-md-6">
-	               		<form action="{{ route('approve.store') }}" >
-	               	{{ csrf_field() }}
-
-	               	<input type="text" hidden>
-	               	<button class="btn btn-xs btn-danger">Cancel</button>
-	               </form>
-	             
+	               		
 	               	</div>
 	               </div>
 	           
 	            </div>
 	        </div>
 	    </div>
+	    	@else
+	    		<div class="panel panel-primary">
+		         <div class="panel-body">
+		         	<p>Nothing to Approve</p>
+		         </div>
+		         </div>
+			@endif
 		@endforeach
 	</div>
 @stop
